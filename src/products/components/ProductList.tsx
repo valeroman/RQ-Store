28ce7851a4adc1f94456333bb85246c726b474f6
valeroman <valeroman@gmail.com>
2024-11-30 13:38:14 -0500

    05-articulos-hombre-mujer

--- src/products/components/ProductList.tsx
+++ src/products/components/ProductList.tsx
@@ -5,11 +5,12 @@ interface Props {
 }
 
 export const ProductList = ({ products }: Props) => {
+
   return (
     <div className="mt-2 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-2 justify-center max-w-max">
 
       {
-        products.map((product) => (
+        products?.map((product) => (
           
           <ProductCard 
             key={product.id}
--- src/products/services/actions.ts
+++ src/products/services/actions.ts
@@ -4,11 +4,23 @@ import { type Product, productApi } from "..";
 
 interface GetProductOpction {
     filterKey?: string;
+};
+
+const sleep = ( seconds: number ):Promise<boolean> => {
+    return new Promise( resolve => {
+        setTimeout(() => {
+            resolve( true );
+        }, seconds * 1000 );
+    })
 }
 
 export const getProducts = async ({ filterKey }: GetProductOpction) => {
+
+    await sleep(2);
     
-    const { data } = await productApi.get<Product[]>('/products');
+    const filterUrl = ( filterKey ) ? `category=${ filterKey }` : '';
+    
+    const { data } = await productApi.get<Product[]>(`/products?${ filterUrl }`);
 
     return data;
 }
