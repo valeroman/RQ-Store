0fdf21df98d9d19bc495e6fc09fbf4a7a36a6c41
valeroman <valeroman@gmail.com>
2024-11-30 19:53:28 -0500

    09-react-hook-form-formulatio

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 ## TanStack Query
 
+ - Documentacion: https://tanstack.com/
+
  - `yarn add @tanstack/react-query`
 
  - Devtools `yarn add @tanstack/react-query-devtoolsyarn add @tanstack/react-query-devtools`
+
+## React Hook Form
+
+- Documentacion: https://www.react-hook-form.com/
+
+- `yarn add react-hook-form`
--- src/products/pages/NewProduct.tsx
+++ src/products/pages/NewProduct.tsx
@@ -1,32 +1,86 @@
 import { Button, Image, Input, Textarea } from "@nextui-org/react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+
+interface FormInputs {
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+};
 
 export const NewProduct = () => {
 
-  
+  const { control, handleSubmit } = useForm<FormInputs>({});
+
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    console.log(data);
+  }
+
 
   return (
     <div className="w-full flex-col">
       <h1 className="text-2xl font-bold">Nuevo producto</h1>
 
-      <form className="w-full">
+      <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
 
         <div className="flex justify-around items-center">
-          
+
           <div className="flex-col w-[500px]">
 
-            <Input className="mt-2" type="text" label="Titulo del producto" />
-            <Input className="mt-2" type="number" label="Precio del producto" />
-            <Input className="mt-2" type="url" label="Url del producto" />
-            <Textarea className="mt-2" label="Descripcion del producto" />
-            <select className="rounded-md p-3 mt-2 bg-gray-800 w-full">
-              <option value="men's clothing">Men's clothing</option>
-              <option value="women's clothing">Women's clothing</option>
-              <option value="jewelery">Jewelery</option>
-              <option value="electronics">Electronics</option>
-            </select>
+            <Controller
+              control={control}
+              name="title"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input value={field.value} onChange={field.onChange} className="mt-2" type="text" label="Titulo del producto" />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="price"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input value={field.value?.toString()} onChange={field.onChange} className="mt-2" type="number" label="Precio del producto" />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="image"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Input value={field.value} onChange={field.onChange} className="mt-2" type="url" label="Url del producto" />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="description"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Textarea value={field.value} onChange={field.onChange} className="mt-2" label="Descripcion del producto" />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="category"
+              rules={{ required: true }}
+              render={({ field }) => (
+                <select value={field.value} onChange={field.onChange} className="rounded-md p-3 mt-2 bg-gray-800 w-full">
+                  <option value="men's clothing">Men's clothing</option>
+                  <option value="women's clothing">Women's clothing</option>
+                  <option value="jewelery">Jewelery</option>
+                  <option value="electronics">Electronics</option>
+                </select>
+              )}
+            />
+
 
             <br />
-            <Button className="mt-2" color="primary">Crear</Button>
+            <Button type="submit" className="mt-2" color="primary">Crear</Button>
           </div>
 
           <div className="bg-white rounded-2xl p-10 flex items-center" style={{
@@ -38,7 +92,7 @@ export const NewProduct = () => {
               src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
             />
           </div>
-          
+
         </div>
 
 
